0c9c9bdae5e11dea0b10d826ecb35f7041394560
herashin <xtick18000@naver.com>
2022-11-11 20:40:40 +0900

    2022-11-11

---
+++ springstudy/NoticeProject/src/main/java/com/gdu/notice/mapper/NoticeMapper.java
@@ -0,0 +1,20 @@
+package com.gdu.notice.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gdu.notice.domain.NoticeDTO;
+
+@Mapper
+public interface NoticeMapper {
+	public List<NoticeDTO> selectAllNotices();
+	public int insertNotice(NoticeDTO notice);
+	
+	//findNoticeByNo
+	public NoticeDTO selectNoticeByNo(int noticeNo);
+	public int updateHit(int noticeNo);
+	public int updateNotice(NoticeDTO notice);
+	public int deleteNotice(int noticeNo);
+	
+}
