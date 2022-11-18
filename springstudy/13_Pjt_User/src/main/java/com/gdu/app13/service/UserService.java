7090f5fa8d1de63d4b763aa5efd81349562a3d4b
herashin <xtick18000@naver.com>
2022-11-18 16:58:24 +0900

    2022-11-18

--- springstudy/13_Pjt_User/src/main/java/com/gdu/app13/service/UserService.java
+++ springstudy/13_Pjt_User/src/main/java/com/gdu/app13/service/UserService.java
@@ -5,20 +5,27 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.gdu.app13.domain.SleepUserDTO;
 import com.gdu.app13.domain.UserDTO;
 
 public interface UserService {
-	
-	public Map<String, Object> isReduceId(String id);
-	public Map<String, Object> isReduceEmail(String email);
-	public Map<String, Object> sendAuthCode(String email);
-	public void join(HttpServletRequest request, HttpServletResponse response);
-	public void retire(HttpServletRequest request, HttpServletResponse response);
-	public void login(HttpServletRequest request, HttpServletResponse response);
-	public void keepLogin(HttpServletRequest request, HttpServletResponse response);
-	public void logout(HttpServletRequest request, HttpServletResponse response);
-	public UserDTO getUserBySessionId(Map<String, Object> map);  // KeepLoginInterceptor에서 호출
-	public Map<String, Object> confirmPassword(HttpServletRequest request);
-	public void modifyPassword(HttpServletRequest request, HttpServletResponse response);
-	
+   
+   public Map<String, Object> isReduceId(String id);
+   public Map<String, Object> isReduceEmail(String email);
+   public Map<String, Object> sendAuthCode(String email);
+   public void join(HttpServletRequest request, HttpServletResponse response);
+   public void retire(HttpServletRequest request, HttpServletResponse response);
+   public void login(HttpServletRequest request, HttpServletResponse response);
+   public void keepLogin(HttpServletRequest request, HttpServletResponse response);
+   public void logout(HttpServletRequest request, HttpServletResponse response);
+   public UserDTO getUserBySessionId(Map<String, Object> map);  // KeepLoginInterceptor에서 호출(원래 서비스는 컨트롤러가 호출하는데 요건 인터셉터가 호출)
+                                                 // 요청 들어가기 전에 인터셉터를 동작시켜야해서
+   public Map<String, Object> confirmPassword(HttpServletRequest request);
+   public void modifyPassword(HttpServletRequest request, HttpServletResponse response);
+   public void sleepUserHandle();  // SleepUserScheduler에서 호출
+   public SleepUserDTO getSleepUserById(String id);
+   public void restoreUser(HttpServletRequest request, HttpServletResponse response);
+   
+   public String getNaverLoginApiURL(HttpServletRequest request);  // 네이버로그인-1
+   public UserDTO getNaverLoginTokenNProfile(HttpServletRequest request);  // 네이버 로그인-2
 }
