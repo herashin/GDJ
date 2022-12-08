7569a0c8ef962e370ea856c3c917987ee7951272
herashin <xtick18000@naver.com>
2022-12-08 21:00:35 +0900

    2022-12-08

--- springstudy/REST/src/main/java/com/gdu/rest/service/MemberService.java
+++ springstudy/REST/src/main/java/com/gdu/rest/service/MemberService.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.gdu.rest.domain.MemberDTO;
 
 public interface MemberService {
-	
-	public Map<String , Object> register(MemberDTO member, HttpServletResponse response);
+	public Map<String, Object> register(MemberDTO member, HttpServletResponse response);
+	public Map<String, Object> getMemberList(int page);
+	public Map<String, Object> getMemberByNo(int memberNo);
 }
